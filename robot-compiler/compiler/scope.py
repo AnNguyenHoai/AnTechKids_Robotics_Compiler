cda1a4d60e5e1e6005d5a8d6f36589f9ab34ec20
AnNguyenHoai <113078343+AnNguyenHoai@users.noreply.github.com>
2026-09-07 12:32:23 +0700

    H29-D: support isolated lexical function scopes

--- robot-compiler/compiler/scope.py
+++ robot-compiler/compiler/scope.py
@@ -2,15 +2,19 @@ from .error import CompilerError
 
 
 class Scope:
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, allocator=None):
         self.parent = parent
         self.variables = {}
+        # Variable indices are program-global even when name lookup is lexical.
+        # A child scope therefore allocates from the same counter as its root
+        # allocator while keeping its own name bindings.
+        self._allocator = allocator if allocator is not None else self
         self.next_index = 0
 
     def allocate(self, name):
         if name not in self.variables:
-            self.variables[name] = self.next_index
-            self.next_index += 1
+            self.variables[name] = self._allocator.next_index
+            self._allocator.next_index += 1
         return self.variables[name]
 
     def resolve(self, name):
